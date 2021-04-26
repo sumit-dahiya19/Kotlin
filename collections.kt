1929ac2b2db38f489a6970c4d3136d0ca9bf4766
Sumit <sumitdahiya1901@gmail.com>
2021-04-26 23:47:31 +0530

    Working on Collections

--- collections.kt
+++ collections.kt
@@ -43,3 +43,54 @@ fun main() {
 
 
 }
+
+//new code from here
+
+fun main() {
+    var numbers= mutableListOf<String>("One","two","three")
+    val newCollections= mutableListOf<String>("1","2","3","4")
+    numbers.add("four")
+    println(numbers)
+    numbers=newCollections
+    println(numbers)
+
+    print(numbers)
+
+    val words = "A long time ago in a galaxy far far away".split(" ")
+    println(words)
+    val shortWords = mutableListOf<String>()
+    words.getShortWordsTo(shortWords, 5)
+    println(shortWords)
+
+    numbers.removeAt(2)
+    numbers.shuffle()
+    print(numbers)
+
+    val set= setOf<Int>(1,2,2,2,3,3)
+    println("number of elements ${set.size}")
+    if(set.contains(2))
+        println("2 is in the set")
+
+    val set2= setOf<Int>(3,1,2)
+    println("${set==set2}")
+    println("$")
+
+    val mp= mutableMapOf<String,Int>("One" to 1,"Two" to 2)
+    mp["Three"]=3
+    mp.put("four",4)
+    print(mp)
+
+
+
+}
+fun print(string:Collection<String>){
+    for(s in string){
+        print("$s")
+    }
+    println(" My function")
+}
+fun List<String>.getShortWordsTo(shortWords: MutableList<String>, maxLength: Int) {
+    this.filterTo(shortWords) { it.length <= maxLength }
+    val articles = setOf("a", "A", "an", "An", "the", "The")
+    shortWords -= articles
+}
